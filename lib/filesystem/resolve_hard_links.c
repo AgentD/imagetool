db25f567c1307a48cfdf75bdb9bc40c7e6147126
David Oberhollenzer <goliath@infraroot.at>
2020-11-09 12:26:00 +0100

    Add fstree hard link resolution test case
    Signed-off-by: David Oberhollenzer <goliath@infraroot.at>

--- lib/filesystem/resolve_hard_links.c
+++ lib/filesystem/resolve_hard_links.c
@@ -13,7 +13,11 @@
 
 int fstree_resolve_hard_links(fstree_t *fs)
 {
-	tree_node_t *n = fs->nodes_by_type[TREE_NODE_HARD_LINK], *target;
+	size_t remaining, resolved;
+	tree_node_t *n, *target;
+
+	/* resolve links */
+	n = fs->nodes_by_type[TREE_NODE_HARD_LINK];
 
 	while (n != NULL) {
 		target = fstree_node_from_path(fs, NULL, n->data.link.target,
@@ -32,8 +36,47 @@ int fstree_resolve_hard_links(fstree_t *fs)
 		}
 
 		n->data.link.resolved = target;
-		target->link_count += 1;
+		n = n->next_by_type;
+	}
 
+	/* recursively resolve links to links */
+	for (;;) {
+		n = fs->nodes_by_type[TREE_NODE_HARD_LINK];
+		remaining = 0;
+		resolved = 0;
+
+		while (n != NULL) {
+			target = n->data.link.resolved;
+
+			if (target->type == TREE_NODE_HARD_LINK) {
+				target = target->data.link.resolved;
+				n->data.link.resolved = target;
+
+				if (target->type == TREE_NODE_HARD_LINK) {
+					remaining += 1;
+				} else {
+					resolved += 1;
+				}
+			}
+
+			n = n->next_by_type;
+		}
+
+		if (remaining == 0)
+			break;
+
+		if (resolved == 0) {
+			fputs("resolving hardlinks: cycle detected!\n", stderr);
+			return -1;
+		}
+	}
+
+	/* compute link count for all nodes */
+	n = fs->nodes_by_type[TREE_NODE_HARD_LINK];
+
+	while (n != NULL) {
+		target = n->data.link.resolved;
+		target->link_count += 1;
 		n = n->next_by_type;
 	}
 
---
+++ tests/resolve_hard_links.c
@@ -0,0 +1,111 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+/*
+ * resolve_hard_links.c
+ *
+ * Copyright (C) 2019 David Oberhollenzer <goliath@infraroot.at>
+ */
+#include "config.h"
+
+#include "fstree.h"
+#include "volume.h"
+#include "test.h"
+
+static volume_t dummy_vol = {
+	.base = {
+		.refcount = 1,
+		.destroy = NULL,
+	},
+
+	.blocksize = 1337,
+
+	.min_block_count = 0,
+	.max_block_count = 10,
+
+	.read_block = NULL,
+	.write_block = NULL,
+	.move_block = NULL,
+	.discard_blocks = NULL,
+	.commit = NULL,
+};
+
+static tree_node_t *mknode(const char *name, int type)
+{
+	tree_node_t *node = calloc(1, sizeof(*node) + strlen(name) + 1);
+
+	TEST_NOT_NULL(node);
+
+	node->type = type;
+	node->name = (const char *)node->payload;
+	strcpy((char *)node->payload, name);
+	return node;
+}
+
+int main(void)
+{
+	tree_node_t *a, *b, *c, *d, *e;
+	tree_node_t *ln0, *ln1, *ln2;
+	fstree_t *fs;
+	int ret;
+
+	/* setup fstree */
+	fs = fstree_create(&dummy_vol, 42);
+	TEST_NOT_NULL(fs);
+
+	/* create a happy little tree */
+	a = mknode("dev", TREE_NODE_DIR);
+	b = mknode("etc", TREE_NODE_DIR);
+	c = mknode("usr", TREE_NODE_DIR);
+	d = mknode("sda0", TREE_NODE_BLOCK_DEV);
+	e = mknode("tty0", TREE_NODE_CHAR_DEV);
+
+	a->parent = fs->root;
+	b->parent = fs->root;
+	c->parent = fs->root;
+	d->parent = a;
+	e->parent = a;
+
+	fs->root->data.dir.children = a;
+	a->next = b;
+	b->next = c;
+	c->next = NULL;
+
+	a->data.dir.children = d;
+	d->next = e;
+	e->next = NULL;
+
+	/* create hard links */
+	ln0 = fstree_add_hard_link(fs, "dev/tty1", "tty0");
+	TEST_NOT_NULL(ln0);
+
+	ln1 = fstree_add_hard_link(fs, "dev/tty2", "/dev/tty1");
+	TEST_NOT_NULL(ln1);
+
+	/* resolve */
+	ret = fstree_resolve_hard_links(fs);
+	TEST_EQUAL_I(ret, 0);
+
+	TEST_ASSERT(ln0->data.link.resolved == e);
+	TEST_ASSERT(ln1->data.link.resolved == e);
+	TEST_EQUAL_UI(e->link_count, 2);
+
+	/* non existant target */
+	ln0 = fstree_add_hard_link(fs, "dev/tty3", "foo");
+	TEST_NOT_NULL(ln0);
+
+	ret = fstree_resolve_hard_links(fs);
+	TEST_ASSERT(ret != 0);
+
+	/* loop */
+	ln1 = fstree_add_hard_link(fs, "dev/foo", "bar");
+	TEST_NOT_NULL(ln1);
+
+	ln2 = fstree_add_hard_link(fs, "dev/bar", "tty3");
+	TEST_NOT_NULL(ln2);
+
+	ret = fstree_resolve_hard_links(fs);
+	TEST_ASSERT(ret != 0);
+
+	/* cleanup */
+	object_drop(fs);
+	return EXIT_SUCCESS;
+}
